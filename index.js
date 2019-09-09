5fe369265592783799d5492015c6e92784d48f12
Arnaldo Souza <arnaldoeloi2@gmail.com>
2019-09-09 11:42:28 -0300

    Limpeza no código.

--- index.js
+++ index.js
@@ -12,26 +12,16 @@ client.once('ready', () => {
 
 client.login(token);
 
-Array.prototype.randomElement = function () {
-    return this[Math.floor(Math.random() * this.length)]
-}
-
-// returns random key from Set or Map
-function getRandomKey(collection) {
-    let keys = Array.from(collection.keys());
-    return keys[Math.floor(Math.random() * keys.length)];
-}
 
 client.on('message', message => {
     if(message.author.bot) return;
     http.get('http://xinga-me.appspot.com/api', (resp) => {
         let data = '';
-        // A chunk of data has been recieved.
+
         resp.on('data', (chunk) => {
             data += chunk;
         });
 
-        // The whole response has been received. Print out the result.
         resp.on('end', () => {
             message.channel.send(`${message.author}, ${JSON.parse(data).xingamento}! `, {
                 tts: true
@@ -41,20 +31,4 @@ client.on('message', message => {
         }).on("error", (err) => {
         console.log("Error: " + err.message);
     });
-
-    // if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    // const args = message.content.slice(prefix.length).split(' ');
-    // const command = args.shift().toLowerCase();
-
-    // console.log(command);
-
-    // if(command === 'spin'){
-
-    //     users = message.channel.members;
-    //     users.delete(message.author.id);
-        
-    //     message.channel.send(`${message.author} spinned the bottle. The bottle is poiting towards ${users.get(getRandomKey(users))}`)
-    // }
-    
 });
